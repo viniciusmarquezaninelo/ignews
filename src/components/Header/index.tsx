f6d21e16f3e8ce59e12af1ae6b469b123493a689
Vinícius Marques Zaninelo <vinicousmarqueszaninelo@gmail.com>
2022-09-23 22:02:00 -0300

    Initial commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+
+import { SingInButton } from '../SignInButton';
+import styles from './styles.module.scss';
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/svgs/logo.svg" alt="ig.news" />
+        <nav>
+          <a href="#" className={styles.active}>Home</a>
+          <a href="#">Posts</a>
+        </nav>
+        <SingInButton />
+      </div>
+    </header>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,53 @@
+import { GetServerSideProps } from 'next';
+
+import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
+
+import styles from './home.module.scss';
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({product}:HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+            <h1>News about the <span>React</span> word.</h1>
+            <p>Get acess to all the publications <br />
+            <span>for {product.amount} month</span>
+          </p>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+        <img src="/svgs/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1LZcr3Gb8a2tJENbhgj5pprU')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    }
+  }
+}
